3ceeaa976e8fab728e0122b879f544d8a57740b7
Stepan <cteger27@gmail.com>
2023-04-10 02:32:55 +0300

    "cpp", "h" and "txt" files

---
+++ Header.h
@@ -0,0 +1,6 @@
+#pragma once
+
+float* Read_abc();
+float Find_discriminant(float* abc);
+float* Find_solution(float* abc, float D);
+void Print_answer(float *x, float D);
--- Quadric/Header.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-float* Read_abc();
-float Find_discriminant(float* abc);
-float* Find_solution(float* abc, float D);
-void Print_answer(float *x, float D);
